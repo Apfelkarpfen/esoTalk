7a12104d19fc2e03ab9a27833ffc360c18ec7933
Apfelkarpfen <jens@datenlaube.net>
2013-12-11 13:41:06 +0100

    German definitions.Likes.php

---
+++ addons/languages/German/definitions.Likes.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// German Definitions for the Profiles plugin.
+
+// Translated from English to German by http://github.com/Apfelkarpfen
+
+$definitions["Unlike"] = "Gef&auml;llt nicht mehr"; // Unlike
+$definitions["Like"] = "Gef&auml;llt"; // Like
+$definitions["Members Who Liked This Post"] = "Mitglieder, denen dieser Beitrag gef&auml;llt"; //Members Who Liked This Post
+$definitions["%s likes this."] = "%s gef&auml;llt das."; // %s likes this.
+$definitions["%s like this."] = "%s gef&auml;llt das."; // %s like this.
