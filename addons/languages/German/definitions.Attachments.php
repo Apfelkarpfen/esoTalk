66dce1da844ed994d83bd6541c564f70c6d67a9b
Apfelkarpfen <jens@datenlaube.net>
2013-12-11 13:35:43 +0100

    German definitions.Attachments.php

---
+++ addons/languages/German/definitions.Attachments.php
@@ -0,0 +1,11 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// German Definitions for the Answers plugin.
+
+// Translated from English to German by http://github.com/Apfelkarpfen
+
+$definitions["Attach a file"] = "Datei anh&auml;ngen"; //Attach a file
+$definitions["Drop files to upload"] = "Dateien zum Hochladen einf&uuml;gen"; //Drop files to upload
+$definitions["message.attachmentNotFound"] = "Aus irgendeinem Grund kann dieser Anhang nicht angezeigt werden. Er k&ouml;nnte gar nicht vorhanden sein, oder Du k&ouml;nntest nicht die Berechtigung haben, ihn anzusehen."; //For some reason this attachment cannot be viewed. It may not exist, or you may not have permission to view it.
