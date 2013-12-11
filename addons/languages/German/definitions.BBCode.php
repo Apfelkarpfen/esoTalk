ddb83ca6ca8f338136fa14cb5778ea22ce3efa2b
Apfelkarpfen <jens@datenlaube.net>
2013-12-11 13:37:57 +0100

    German definitions.BBCode.php

---
+++ addons/languages/German/definitions.BBCode.php
@@ -0,0 +1,15 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// German definitions for the BBCode plugin.
+
+// Translated from English to German by http://github.com/Apfelkarpfen
+
+$definitions["Code"] = "Quellcode"; // Code
+$definitions["Image"] = "Bild"; // Image
+$definitions["Link"] = "Verweis"; // Link
+$definitions["Strike"] = "Durchstreichen"; // Strike
+$definitions["Header"] = "&Uuml;berschrift"; // Header
+$definitions["Italic"] = "Kursiv"; // Italic
+$definitions["Bold"] = "Fett"; // Bold
