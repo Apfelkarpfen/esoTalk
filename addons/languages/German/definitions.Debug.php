43b598eab484000ce546c96fb9dff245c8ddf531
Apfelkarpfen <jens@datenlaube.net>
2013-12-11 13:39:34 +0100

    German definitions.Debug.php

---
+++ addons/languages/German/definitions.Debug.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// German definitions for the Debug plugin.
+
+// Translated from English to German by http://github.com/Apfelkarpfen
+
+$definitions["MySQL queries"] = "MySQL Abfragen"; // MySQL queries
+$definitions["Page loaded in %s seconds"] = "Seite in %s Sekunden geladen"; // Page loaded in %s seconds
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES Information"; // POST + GET + FILES information
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE Information"; // SESSION + COOKIE information
